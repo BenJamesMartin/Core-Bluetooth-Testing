163b0e4b00affcb17784e380ff183579c9396359
Ben Martin <ben@martinben.com>
2015-03-11 17:19:52 -0700

    Create initial View Controller that will make the phone initially become a peripheral and only become a central as needed

---
+++ Bluetooth Test/InitialViewController.h
@@ -0,0 +1,15 @@
+//
+//  InitialViewController.h
+//  Bluetooth Test
+//
+//  Created by Benjamin Martin on 3/11/15.
+//  Copyright (c) 2015 Benjamin Martin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InitialViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UILabel *dataLabel;
+
+@end
